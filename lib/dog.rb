c5d01ef6ddd1171f37cdae233f9d70e06c2f996f
lorain <laureen.akinyi@student.moringaschool.com>
2022-08-20 15:24:54 +0300

    complete assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Dog
+    #setter method
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breeds)
+        @breed = breeds
+    end
+
+    #getter method
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 
 end
+
+lassie = Dog.new
+lassie.name=('Lassie')
+
+pug = Dog.new
+pug.breed=("Pug")
+
+binding.pry
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Person
+    #setter method
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    #getter method
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 
 end
+
+akinyi = Person.new
+akinyi.name=("Akinyi")
+
+pilot = Person.new
+pilot.job=("Pilot")
+
+binding.pry
